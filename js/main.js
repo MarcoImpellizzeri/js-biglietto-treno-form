adc35c9d877669f407e0cfce8993f125e88db195
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-03 16:54:22 +0200

    mileston 1 done

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-const totKm = prompt("Inserisci quanti chilometri vuoi percorrere");
-const age = prompt("Inserisci la tua etá");
-let priceTiket = (parseInt(totKm) * 0.21).toFixed(2);
+const userNameInputElement = document.querySelector("#user-name");
+const totKmInputElement = document.querySelector("#user-Km");
+const ageInputElement = document.querySelector("#user-age");
+const buttonGen = document.querySelector("#generate-tiket")
 
-if (parseInt(age) < 18) {
-    console.log((priceTiket - (priceTiket * 20) / 100 ));
-} else if (parseInt(age) >= 65) {
-    console.log((priceTiket - (priceTiket * 40) / 100 ));
-} else {
-    console.log(priceTiket);
-}
+
+buttonGen.addEventListener("click", function () {
+
+    const username = userNameInputElement.value
+    const totKm = totKmInputElement.value
+    const age = ageInputElement.value
+    let priceTiket = (totKm * 0.21);
+
+    if (parseInt(age) < 18) {
+        document.getElementById("tot-price").innerHTML = (priceTiket - (priceTiket * 20) / 100).toFixed(2);
+        ((priceTiket - (priceTiket * 20) / 100));
+    } else if (parseInt(age) >= 65) {
+        document.getElementById("tot-price").innerHTML = ((priceTiket - priceTiket * 40) / 100).toFixed(2);
+        console.log((priceTiket - (priceTiket * 40) / 100));
+    } else {
+        document.getElementById("tot-price").innerHTML = priceTiket.toFixed(2);
+        console.log(priceTiket);
+    }
+})
