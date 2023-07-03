f519c728d323771422bb85f83ad4ca25015c06d6
MarcoImpellizzeri <impellizzeri.marco18@gmail.com>
2023-07-03 18:30:12 +0200

    milestone 2 done + bonus 1

--- js/main.js
+++ js/main.js
@@ -2,32 +2,35 @@ const userNameInputElement = document.querySelector("#user-name");
 const totKmInputElement = document.querySelector("#user-Km");
 const ageInputElement = document.querySelector("#user-age");
 const buttonGen = document.querySelector("#generate-tiket")
-
+const buttonReload = document.querySelector("#reload")
 
 buttonGen.addEventListener("click", function () {
-    const username = userNameInputElement.value
+    let username = userNameInputElement.value
     const totKm = totKmInputElement.value
     const age = ageInputElement.value
     let priceTiket = (totKm * 0.21);
     const minVagon = 1;
     const maxVagon = 10;
-    var randomNumVagon = Math.floor(Math.random() * (maxVagon - minVagon + 1)) + minVagon;
+    let randomNumVagon = Math.floor(Math.random() * (maxVagon - minVagon + 1)) + minVagon;
     const minCode = 9999;
     const maxCode = 100000;
-    var randomNumCode = Math.floor(Math.random() * (maxCode - minCode + 1)) + minCode;
+    let randomNumCode = Math.floor(Math.random() * (maxCode - minCode + 1)) + minCode;
 
     document.getElementById("vagon").innerHTML = randomNumVagon
     document.getElementById("code-cp").innerHTML = randomNumCode
-    document.getElementById("user-name").innerHTML = username;
-
+    document.getElementById("user-name-tiket").innerHTML = username;
 
     if (parseInt(age) < 18) {
         document.getElementById("offer").innerHTML = `Sconto 20%`;
         document.getElementById("tot-price").innerHTML = (priceTiket - (priceTiket * 20) / 100).toFixed(2);
     } else if (parseInt(age) >= 65) {
         document.getElementById("offer").innerHTML = `Sconto 40%`;
-        document.getElementById("tot-price").innerHTML = ((priceTiket - priceTiket * 40) / 100).toFixed(2);
+        document.getElementById("tot-price").innerHTML = (priceTiket - (priceTiket * 40) / 100).toFixed(2);
     } else {
         document.getElementById("tot-price").innerHTML = priceTiket.toFixed(2);
     }
+})
+
+buttonReload.addEventListener("click", function(){
+    location.reload();
 })
